ee4824fb47f91ec462716fd6df3854c776f446b0
MelissaB79 <bertinmelissa@hotmail.fr>
2022-03-24 16:47:34 +0100

    init commit

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,44 +1,61 @@
 package controllers;
 
-import play.mvc.Controller;
+import models.Tache;
+import play.mvc.*;
+import java.util.*;
+import play.db.jpa.JPA;
+import javax.persistence.Query;
+import play.data.validation.*;
 
 public class Application extends Controller {
 
     // Affiche toutes les tâches (voir variable taches) dans le template views/listTache.html
     public static void listTache() {
-        // A COMPLETER
-        // ...
-		render();
+        //List<Tache> taches = null;
+        //try {
+        //    Query query = JPA.em().createQuery("select from Tache");
+        //    taches = query.getResultList();
+        //} catch (Exception e) {
+        //    System.err.println(e);
+        //}
+        //EntityManager em =  play.db.jpa.JPA.em();
+        List<Tache> taches = Tache.findAll();
+        render(taches);
     }
 
     // Affiche le template views/ajouterTacheForm.html (formulaire d'ajout d'une tâche)
     public static void ajouterTacheForm() {
-        // A COMPLETER
-        // ...
+        render();
     }
 
     // Ajoute une nouvelle tâche en base de données et affiche le template views/ajouterTache.html
-    public static void ajouterTache() {
-        // A COMPLETER
-        // ...
+    public static void ajouterTache(Tache tache) {
+        validation.required(tache.title);
+        if(validation.hasErrors()) {
+            params.flash(); // add http parameters to the flash scope
+            validation.keep(); // keep the errors for the next request
+            render();
+        }
+        tache.save();
+        render();
     }
 
     // Change le statut d'une tâche en base de données
     public static void validerTache(Long id) {
     	// A COMPLETER
-        // ...   
+        // ...
     }
 
     // Supprime une tâche en base de données
     public static void supprimerTache(Long id) {
-        // A COMPLETER
-        // ...
+        Tache tache = Tache.findById(id);
+        tache.delete();
+        listTache();
     }
 
     // Modifie une tâche en base de données
     public static void editTache(Long id, String title) {
-        // A COMPLETER
-        // ...
+        System.out.println(title);
     }
 
 }
--- app/models/Tache.java
+++ app/models/Tache.java
@@ -1,7 +1,23 @@
 package models;
 
-import play.db.jpa.Model;
+import play.db.jpa.*;
+import javax.persistence.*;
 
+@Entity
 public class Tache extends Model {
+    @Id
+    @GeneratedValue
+    public long id;
+    public String title;
+    public boolean isValidate;
+
+    public Tache(String title){
+        this.title = title;
+        this.isValidate = false;
+    }
+
+    public String toString(){
+        return this.id + " : " + this.title;
+    }
 	
 }
